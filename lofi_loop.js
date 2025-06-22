31809812dcd3aa1ca7f1699bbdd74e074e686269
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-06-21 19:38:08 -0700

    Verse and vocals

--- lofi_loop.js
+++ lofi_loop.js
@@ -1,21 +1,54 @@
+samples('shabda/speech:consciousness,quantum')
+
+$: arrange(
+  [8, note("c1").s("consciousness").loopAt(choose(0.3, 0.25)).scrub("<0@3 0.48!2 ~>*3")._punchcard({width: 600})],
+  [4, note("<c1>*2").s("quantum")],
+  [4, "~"],
+  [4, note("<g1>*1").s("quantum")],
+  [4, "~"],
+  [4, note("<c1>*2").s("quantum")],
+  [8, note("c1").s("consciousness").loopAt(choose(0.3, 0.25)).scrub("<0@3 0.48!2 ~>*3")._punchcard({width: 600})],
+)
+
 $: note(arrange(
-  [4, `<c4 e4 g4 b4 c4 e4 g4 b4>*4`],
+  [4, `g5 | e5 | c5 | b5 | g5`],
+  [4, `g6 | e6 | c6 | b6 | g6`],
+  [4, `<c4 e4 g4 b4>*4`],
   [4, stack(`<c3 e3 g3 b3 c3 e3 g3 c4 c3 e3 g3 b3 c3 e3 g3 g4>*8`, 
-              `<c3 e3 g3 b3 c3 e3 g3 c4 c3 e3 g3 b3 c3 e3 g3 g4>*8`.add(`5`))]
-)).sound("piano").room(0.8).size(3).lpf(800)._scope()
+              `<c3 e3 g3 b3 c3 e3 g3 c4 c3 e3 g3 b3 c3 e3 g3 g4>*8`.add(`5`))],
+  [4, `<c4 e4 g4 b4>*4`],
+  [4, stack(`<b4 g4 e4 c4 c4 g4 e4 c5 b4 g4 e4 c4 c4 e4 g4 g5>*8`, 
+              `<b4 g4 e4 c4 c4 g4 e4 c5 b4 g4 e4 c4 c4 e4 g4 g5>*8`.add(`5`))],
+  [4, stack(`<g5 e5 c5 b5 g5 e5 c5 b5 g5 c5 e5 b5 g4 e4 g4 g5>*8`, 
+              `<b4 g4 e4 c4 c4 g4 e4 c5 b4 g4 e4 c4 c4 e4 g4 g5>*8`.add(`5`))],
+  [4, `g5 | e5 | c5 | b5 | g5`],
+  [4, `g6 | e6 | c6 | b6 | g6`]
+)).sound("piano").lpf(2000)._scope()
     
 $: note(arrange(
+  [8, `<~ ~ ~ ~ ~ ~ ~ c4>*4 | <~ ~ ~ ~ ~ ~ ~ g4>*4`],
   [4, `<b4 c4 e4 f4 a5>*8`],
-  [4, `<a2 f3 e3 c3 g4>*8`]
-)).sound("square").rarely(ply("2 | 4 | 8")).room(0.2).size(3).lpf(slider(727,0,1000,1)).lpa(.2)._scope()
+  [4, `<a2 f3 e3 c3 g4>*8`],
+  [4, `<b4 c4 e4 f4 a5>*8`],
+  [4, `<a2 f3 e3 g4>*8`],
+  [4, `<a2 f3 e3 g4>*8`],
+  [8, `~`],
+)).sound(choose("square", "sawtooth")).rarely(ply("2 | 4 | 8")).size(3).lpf(slider(1000,0,1000,1)).lpa(.2)._scope()
 
-$: note("<[c3,g3,c4] [g3,b3,d3]>").s("gm_electric_guitar_clean").room(2).lpf(slider(280,0,1000,1))._scope()
+$: note("<[c3,g3,c4] [g3,b3,d3]>").s("gm_electric_guitar_clean").lpf(slider(1000,0,1000,1))._scope()
 
-$: sound("<bd hh bd rim>*4").rarely(ply("2 | 4")).lpf(slider(659,0,1000,1))._scope()
+$: sound(arrange(
+  [8, "<bd hh bd hh>*4"],
+  [20, "<bd hh bd rim>*4"],
+  [8, "<bd hh bd hh>*4"]
+)).rarely(ply("2 | 4")).lpf(slider(1000,0,1000,1))._scope()
 
 $: note(arrange(
-  [4, "<[c2 e2 g2]*2 [g1 b1 d2]*2>"],
-  [4, "<[c2 e2 g2]*2 [g1 b1 d2]*2 [e2 g2 b2]*2 [c3 e3 g3]*2>"]
-)).sound("gm_synth_bass_1").lpf(slider(610,0,1000,1))._scope()
-
-$: note("<[g3,b3,e4]!2 [a3,c3,e4] [b3,d3,b4]>").sound("sawtooth").room(1).size(2).lpf(slider(735,0,1000,1))._scope()
+  [8, "<[c2 e2 c2 g2]*2 [g1 b1 g1 d2]*2 [e2 g2 e2 b2]*2 [c3 e3 c3 g3]*2>"],
+  [4, "<[c2 e2 c2 g2]*2 [g1 b1 g1 d2]*2 [e2 g2 e2 b2]*2 [c3 e3 c3 g3]*2>"],
+  [4, "<[c2 g2]*2 [g1 d2]*2>"],
+  [4, "<[c2 e2 c2 g2]*2 [g1 b1 g1 d2]*2 [e2 g2 e2 b2]*2 [c3 e3 c3 g3]*2>"],
+  [4, "<[c2 g2]*2 [g1 d2]*2>"],
+  [4, "<[c2 g2]*2 [g1 d2]*2>"],
+  [8, "<[c2 e2 c2 g2]*2 [g1 b1 g1 d2]*2 [e2 g2 e2 b2]*2 [c3 e3 c3 g3]*2>"],
+)).sound("gm_synth_bass_1").lpf(slider(1500,0,1500,1))._scope()
